e21c370c189c7d8238e9d6fe3ec1605a6602b19a
Nikola Barać <nikola.barac12345@gmail.com>
2024-02-14 15:20:58 +0100

    uploaded dataframe and csv (maybe)

---
+++ jobs_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 15:01:08 2024
+
+@author: Nikola
+"""
+
+import pandas as pd
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv('D:/Python_e/ds_salary_proj/glassdoor_jobs.csv')
+
+# Display the DataFrame
+print(df)
